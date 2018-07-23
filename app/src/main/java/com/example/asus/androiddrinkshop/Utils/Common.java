e06aededfa2c5670221153d37b16cf5212378555
asus <asus@DESKTOP-8MMABT9>
2018-07-23 15:19:21 +0700

    part 6 Menambahakn List Drink

--- app/src/main/java/com/example/asus/androiddrinkshop/Utils/Common.java
+++ app/src/main/java/com/example/asus/androiddrinkshop/Utils/Common.java
@@ -1,5 +1,6 @@
 package com.example.asus.androiddrinkshop.Utils;
 
+import com.example.asus.androiddrinkshop.Model.Category;
 import com.example.asus.androiddrinkshop.Model.User;
 import com.example.asus.androiddrinkshop.Retrofit.IDrinkShopAPI;
 import com.example.asus.androiddrinkshop.Retrofit.RetrofitClient;
@@ -7,6 +8,8 @@ import com.example.asus.androiddrinkshop.Retrofit.RetrofitClient;
 public class Common {
     private static final String BASE_URL = "https://drinkshopapi.000webhostapp.com/";
     public static User currentUser = null;
+
+    public static Category currentCategody = null;
 
     public static IDrinkShopAPI getApi(){
         return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
